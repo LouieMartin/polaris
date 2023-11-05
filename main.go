3f400bcb7ba135ed56d5e1b26fde5dc20f9fe173
LouieMartin <louie.decierdo@gmail.com>
2023-11-05 10:47:27 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,32 +1,17 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"strings"
+
+	"github.com/notnil/chess"
 
 	"github.com/LouieMartin/polaris/engine"
-	"github.com/notnil/chess"
 )
 
 func main() {
 	var game *chess.Game = chess.NewGame()
 
 	for game.Outcome() == chess.NoOutcome {
-		fmt.Print("Enter your move: ")
-		reader := bufio.NewReader(os.Stdin)
-		move, err := reader.ReadString('\n')
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if err := game.MoveStr(strings.TrimSpace(move)); err != nil {
-			log.Fatal(err)
-		}
-
 		engine.PlayBestMove(3, game)
 
 		fmt.Println(game.Position().Board().Draw())
